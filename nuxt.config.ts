c1f245de7a5585d898c1c4a576c2edcdd8352560
Hamza Mir <hamza.mir66@gmail.com>
2024-05-10 12:42:49 +0500

    5.3 init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ import { resolveAlias } from "nuxt/kit";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	store: {
+		// Path to your Vuex store (usually store/index.js)
+		path: "./store/index.js",
+	},
 });
